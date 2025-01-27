bebae937c31f3187315ac09d1e5fa0baa8559b3a
David Weiss <davidwe@mta.ac.il>
2025-01-27 12:54:30 +0200

    refactor naming and other conventions, text collate is irrelevant for posgtresql

--- shifts.sql
+++ shifts.sql
@@ -1,121 +1,122 @@
-CREATE TABLE general_address (
-address_id SERIAL PRIMARY KEY,
-address_country VARCHAR(255) NOT NULL,
-address_city VARCHAR(255) NOT NULL,
-address_street VARCHAR(255) NOT NULL,
-postal_code varchar(100),
-address_additional_info TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL
+CREATE TABLE general_addresses (
+address_id serial PRIMARY KEY,
+address_country varchar(100) NULL DEFAULT NULL,
+address_city varchar(100) NULL DEFAULT NULL,
+address_street varchar(100) NULL DEFAULT NULL,
+postal_code varchar(20) NULL DEFAULT NULL,
+address_additional_info text NULL DEFAULT NULL
 );
 
-CREATE TABLE org_role (
-role_id SERIAL PRIMARY KEY,
-role_name VARCHAR(255) NOT NULL,
-role_description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL
+CREATE TABLE organization_roles (
+role_id serial PRIMARY KEY,
+role_name varchar(100) NOT NULL,
+role_description text NULL DEFAULT NULL
 );
 
-CREATE TABLE org_admin (
-admin_id SERIAL PRIMARY KEY
+CREATE TABLE organization_admins (
+admin_id serial PRIMARY KEY
 );
 
-CREATE TABLE org_permission (
-permission_id SERIAL PRIMARY KEY,
-permission_admin_id INTEGER,
-permission_text TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-FOREIGN KEY (permission_admin_id) REFERENCES org_admin(admin_id)
+CREATE TABLE organization_permissions (
+permission_id serial PRIMARY KEY,
+permission_admin_id_fkey integer NOT NULL,
+permission_name varchar(100) NOT NULL,
+permission_text text  NULL DEFAULT NULL,
+FOREIGN KEY (permission_admin_id_fkey) REFERENCES organization_admins(admin_id)
 );
 
-CREATE TABLE employee (
-employee_id SERIAL PRIMARY KEY,
-employee_name VARCHAR(255) NOT NULL,
-employee_email VARCHAR(255) NOT NULL,
-employee_phone VARCHAR(20) NOT NULL,
-employee_password VARCHAR(255) NOT NULL,
-employee_role_id INTEGER,
-employee_address_id INTEGER,
-employee_is_admin INTEGER,
-FOREIGN KEY (employee_role_id) REFERENCES org_role(role_id),
-FOREIGN KEY (employee_address_id) REFERENCES general_address(address_id),
-FOREIGN KEY (employee_is_admin) REFERENCES org_admin(admin_id)
+CREATE TABLE employees (
+employee_id serial PRIMARY KEY,
+employee_name varchar(255) NOT NULL,
+employee_email varchar(255) NOT NULL,
+employee_phone varchar(20) NULL DEFAULT NULL,
+employee_password varchar(255) NOT NULL,
+employee_role_id_fkey integer NULL DEFAULT NULL,
+employee_address_id_fkey integer NULL DEFAULT NULL,
+employee_is_admin_fkey integer NULL DEFAULT NULL,
+FOREIGN KEY (employee_role_id_fkey) REFERENCES organization_roles(role_id),
+FOREIGN KEY (employee_address_id_fkey) REFERENCES general_addresses(address_id),
+FOREIGN KEY (employee_is_admin_fkey) REFERENCES organization_admins(admin_id)
 );
 
-CREATE TABLE organization(
-organization_id SERIAL PRIMARY KEY,
-organization_name VARCHAR(255) NOT NULL,
-organization_password VARCHAR(255) NOT NULL
+CREATE TABLE organizations(
+organization_id serial PRIMARY KEY,
+organization_name varchar(255) NOT NULL,
+organization_password varchar(255) NOT NULL
 );
 
-CREATE TABLE organization_group(
-    organization_group_id SERIAL PRIMARY KEY,
-    organization_group_name VARCHAR(255) NOT NULL
+CREATE TABLE organization_groups(
+    organization_group_id serial PRIMARY KEY,
+    organization_group_name varchar(100) NOT NULL
 );
 
 CREATE TABLE employee_groups (
-    etog_group_id INTEGER NOT NULL,
-    etog_employee_id INTEGER NOT NULL,
-    PRIMARY KEY (etog_group_id, etog_employee_id),
-    FOREIGN KEY (etog_employee_id) REFERENCES employee(employee_id),
-    FOREIGN KEY (etog_group_id) REFERENCES org_group(org_group_id)
+    etog_group_id_fkey integer NOT NULL,
+    etog_employee_id_fkey integer NOT NULL,
+    PRIMARY KEY (etog_group_id_fkey, etog_employee_id_fkey),
+    FOREIGN KEY (etog_employee_id_fkey) REFERENCES employees(employee_id),
+    FOREIGN KEY (etog_group_id_fkey) REFERENCES organization_groups(organization_group_id)
 );
 
-CREATE TABLE announcment (
-    announcment_id SERIAL PRIMARY KEY,
-    announcment_title VARCHAR(255) NOT NULL,
-    announcment_date DATE
+CREATE TABLE announcments (
+    announcment_id serial PRIMARY KEY,
+    announcment_title varchar(100) NOT NULL,
+    announcment_text text,
+    announcment_date date NULL DEFAULT NULL
 );
 
 CREATE TABLE recipients (
-    recipients_group_id INTEGER NOT NULL,
-    `recipients_announcment_id` int NOT NULL,
-    PRIMARY KEY (recipients_group_id, recipients_announcment_id),
-    FOREIGN KEY (recipients_group_id) REFERENCES org_group(org_group_id),
-    FOREIGN KEY (recipients_announcment_id) REFERENCES announcment(announcment_id)
+    recipients_group_id_fkey integer NOT NULL,
+    recipients_announcment_id_fkey integer NOT NULL,
+    PRIMARY KEY (recipients_group_id_fkey, recipients_announcment_id_fkey),
+    FOREIGN KEY (recipients_group_id_fkey) REFERENCES organization_groups(organization_group_id),
+    FOREIGN KEY (recipients_announcment_id_fkey) REFERENCES announcments(announcment_id)
 );
 
-CREATE TABLE department (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(255),
-    department_address_id INTEGER,
-    department_manager_id INTEGER,
-    FOREIGN KEY (department_address_id) REFERENCES general_address(address_id),
-    FOREIGN KEY (department_manager_id) REFERENCES employee(employee_id)
+CREATE TABLE departments (
+    department_id serial PRIMARY KEY,
+    department_name varchar(100) NOT NULL,
+    department_address_id_fkey integer NULL DEFAULT NULL,
+    department_manager_id_fkey integer NULL DEFAULT NULL,
+    FOREIGN KEY (department_address_id_fkey) REFERENCES general_addresses(address_id),
+    FOREIGN KEY (department_manager_id_fkey) REFERENCES employees(employee_id)
 );
 
-CREATE TABLE sub_department (
-    sub_department_id SERIAL PRIMARY KEY,
-    sub_department_name VARCHAR(255) NOT NULL,
-    sub_department_capacity INTEGER,
-    main_department_id INTEGER,
-    sub_department_manager_id INTEGER,
-    FOREIGN KEY (main_department_id) REFERENCES department(department_id),
-    FOREIGN KEY (sub_department_manager_id) REFERENCES employee(employee_id)
+CREATE TABLE sub_departments (
+    sub_department_id serial PRIMARY KEY,
+    sub_department_name varchar(100) NOT NULL,
+    sub_department_capacity integer NULL DEFAULT NULL,
+    main_department_id_fkey integer NOT NULL,
+    sub_department_manager_id_fkey integer NULL DEFAULT NULL,
+    FOREIGN KEY (main_department_id_fkey) REFERENCES departments(department_id),
+    FOREIGN KEY (sub_department_manager_id_fkey) REFERENCES employees(employee_id)
 );
 
-CREATE TABLE avaliable_shift (
-    shift_id SERIAL PRIMARY KEY,
-    shift_date DATE NULL DEFAULT NULL,
-    shift_time_start TIME(7),
-    shift_time_end TIME(7),
-    sub_department_id INTEGER,
-    FOREIGN KEY (sub_departmentid) REFERENCES sub_department(sub_department_id)
+CREATE TABLE avaliable_shifts (
+    shift_id serial PRIMARY KEY,
+    shift_date date NULL DEFAULT NULL,
+    shift_time_start time(6) NULL DEFAULT NULL,
+    shift_time_end time(6) NULL DEFAULT NULL,
+    sub_department_id_fkey integer NULL DEFAULT NULL,
+    FOREIGN KEY (sub_department_id_fkey) REFERENCES sub_departments(sub_department_id)
 );
 
-CREATE TABLE assigned_shift (
-    assigned_id SERIAL PRIMARY KEY,
-    assigned_shift_id INTEGER NOT NULL,
-    assigned_employee_id INTEGER NOT NULL,
-    FOREIGN KEY (assigned_shift_id) REFERENCES avaliable_shift(shift_id),
-    FOREIGN KEY (assigned_employee_id) REFERENCES employee(employee_id)
+CREATE TABLE assigned_shifts (
+    assigned_id serial PRIMARY KEY,
+    assigned_shift_id_fkey integer NOT NULL,
+    assigned_employee_id_fkey integer NOT NULL,
+    FOREIGN KEY (assigned_shift_id_fkey) REFERENCES avaliable_shifts(shift_id),
+    FOREIGN KEY (assigned_employee_id_fkey) REFERENCES employees(employee_id)
 );
 
-CREATE TYPE request_status AS ENUM ('pending', 'approved', 'denied')
+CREATE TYPE request_status AS ENUM ('pending', 'approved', 'denied');
 
-CREATE TABLE requested_shift (
-    request_id SERIAL PRIMARY KEY,
-    request_shift_id INTEGER NOT NULL,
-    request_employee_id INTEGER NOT NULL,
-    request_notes TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
-    request_status VARCHAR(255),
-    FOREIGN KEY (request_shift_id) REFERENCES avaliable_shift(shift_id),
-    FOREIGN KEY (request_employee_id) REFERENCES employee(employee_id)
+CREATE TABLE requested_shifts (
+    request_id serial PRIMARY KEY,
+    request_shift_id integer NOT NULL,
+    request_employee_id integer NOT NULL,
+    request_notes text NULL DEFAULT NULL,
+    request_status request_status DEFAULT 'pending',
+    FOREIGN KEY (request_shift_id) REFERENCES avaliable_shifts(shift_id),
+    FOREIGN KEY (request_employee_id) REFERENCES employees(employee_id)
 );
-
